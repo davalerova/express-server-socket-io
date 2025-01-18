d338125d9134081b44e47dd1b070a4391f27d3c2
David Valero <dvdovni@gmail.com>
2025-01-17 22:36:27 -0500

    Socket: Emitir bandas registradas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,23 @@
 const { io } = require('../index'); // Se importa io de index.js
 
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+
+const bands = new Bands();
+
+bands.addBand(new Band('Metallica'));
+bands.addBand(new Band('Epica'));
+bands.addBand(new Band('Pearl Jam'));
+bands.addBand(new Band('Red Hot Chili Peppers'));
+
+console.log(bands.getBands());
+
+
 
 io.on('connection', client => {
     console.log('Cliente conectado');
+    client.emit('active-bands', bands.getBands());
     client.on('disconnect', () => { 
         console.log('Cliente desconectado');
     });
